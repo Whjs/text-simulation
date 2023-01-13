52d07edd4e9777a5271b4ea3ac96f5145e9de549
fanbin <fanb7428@joyowo.com>
2023-01-13 17:53:23 +0800

    基础架构设定

--- src/views/Help/index.js
+++ src/views/Help/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import './index.less'
+
 function Help() {
   return (
-    <div>
-      Help
+    <div className='help-container'>
+      <h3>农场</h3>
+      <ul>
+        <li>1 - 10级</li>
+        <li>员工称号：农场学徒(初、中、高级)；农场工程师(初、中、高级)；农场大师(初、中、高级)</li>
+      </ul>
+      <h3>矿场</h3>
+      <ul>
+        <li>1 - 10级</li>
+        <li>员工称号：矿场学徒(初、中、高级)；矿场工程师(初、中、高级)；矿场大师(初、中、高级)</li>
+      </ul>
     </div>
   )
 }
